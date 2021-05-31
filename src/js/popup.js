1c412952bcee9a5a1f17228a10400253ec57ac9e
Nikolaos P Argalias <nikosgalias@gmail.com>
2021-05-31 18:24:36 +0100

    added close popup on Esc keypress

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,11 +1,25 @@
 const bookBtn = document.querySelectorAll(".book-now");
 const popup = document.getElementById("popup");
 const popupClose = document.getElementById("popup__close");
+const popupOverlay = document.getElementById("popup__overlay");
 
+// Open popup on click book button
 bookBtn.forEach((btn) => {
   btn.onclick = openPopup;
 });
+
+// close popup on click X btn
 popupClose.onclick = closePopup;
+
+// close popup on click outside modal
+popupOverlay.onclick = closePopup;
+
+// close poup on escape key
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && popup.classList.contains("popup__visible")) {
+    closePopup();
+  }
+});
 
 function closePopup() {
   popup.classList.remove("popup__visible");
